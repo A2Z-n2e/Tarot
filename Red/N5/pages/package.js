7762c97f3c03401d9da0c913d3ab7244dbff16cf
A2Z-n2e <627315447@qq.com>
2025-07-04 14:27:09 +0800

    json 优化版本

---
+++ Red/N5/pages/package.js
@@ -0,0 +1,8 @@
+var student = [
+    {name: '鲁班七号', gender: '未知', age: '20', class: '一年级射手班', note: '智商二百五的电玩小子，biubiubiu，就是腿短！'},
+    {name: '安琪拉', gender: '女', age: '18', class: '一年级法师班', note: '什么什么魔法书！'},
+    {name: '程咬金', gender: '男', age: '30', class: '一年级二班', note: '爱与正义之斧，偷偷推到'},
+    {name: '阿珂', gender: '女', age: '19', class: '三年级刺客班', note: '哪里亮了点哪里，都是我的人头！'},
+    {name: '李白', gender: '男', age: '20', class: '一年级酒酒仙班', note: '但愿长醉不愿醒，我是来无影去无踪，帅!'},
+    {name: '森瑶', gender: '女', age: '20', class: '皮肤班级', note: '不然梦会碎还是破！'}
+]
